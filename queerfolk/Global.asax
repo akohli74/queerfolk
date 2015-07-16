85bf2205852b1822dd0a2e0f72ac42fe1a5f4281
Amrit Kohli <info@amritkohli.net>
2015-07-15 18:47:19 -0700

    initial MVC Commit

---
+++ queerfolk/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="queerfolk.MvcApplication" Language="C#" %>
---
+++ queerfolk/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(queerfolk.Startup))]
+namespace queerfolk
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
